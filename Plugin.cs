17887cf43b5cb9b3a19cd32be924ae2d5ee3fe0b
arielthemonke <arielsaks1@gmail.com>
2025-05-01 15:51:13 +0300

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+
+namespace GorillaStopWatch
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.ProjectName, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(Init);
+        }
+
+        void Init()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GorillaStopWatch
+{
+
+    // This class is used to store information about the plugin. Please fill in the fields below.
+    class PluginInfo
+    {
+        public const string GUID = "arielthemonke.gorillastopwatch";
+        public const string ProjectName = "GorillaStopWatch";
+        public const string Version = "1.0.0";
+    }
+}
